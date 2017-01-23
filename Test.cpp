abedbae5e9a3ddda5443f5cd74d2edab02abf34d
liaojianqi <loinliao@163.com>
2017-01-23 15:59:31 +0800

    测试类

--- Test.cpp
+++ Test.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include "server.h"
 using namespace webserver;
-
+ 
 int main(){
+    unsigned int port = 8888;
     //1. 初始化
-    Server server(8080);
+    Server server(port);
     //2. 设置路由
     //可以从req对象获取请求信息，将响应信息写入out对象即可，无需手动清空缓冲
     server.router["index.html"]["GET"] = [](const Request &req, std::ostream &out){
         out<<"<html><head></head><body><h1>This is index GET page!</h1></body></html>";
     };
-    server.router["login.html"][""] = [](const Request &req, std::ostream &out){
+    server.router["login.html"]["GET"] = [port](const Request &req, std::ostream &out){
         out<<"<html><head></head><body><h1>This is login GET page!</h1>"
-            "<form action='http://127.0.0.1:port/doLogin.html' method='post'>"
+            "<form action='http://127.0.0.1:" << port << "/doLogin.html' method='post'>"
             "username:<input type='text' name='username'><br>"
-            "password:<input type='password' name='username'><br>"
+            "password:<input type='password' name='password'><br>"
             "<input type='submit'>"
             "</form></body></html>";
     };
@@ -30,7 +31,7 @@ int main(){
             <<"<p>Your password is "<<password<<"</p></body></html>";
     };
     server.default_router["GET"] = [](const Request &req, std::ostream &o){
-        return "<html><head></head><body><h1>This is default GET page!</h1></body></html>";
+        o<<"<html><head></head><body><h1>This is default GET page!</h1></body></html>";
     };
     //3. 启动服务器
     server.start();
