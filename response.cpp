fb487406f3b2efe3852a0648d71bd0ebdfc1f9e4
liaojianqi <loinliao@163.com>
2017-01-24 23:26:38 +0800

    增加response.cpp文件，分离声明和实现

---
+++ response.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include "boost/asio.hpp"
+#include "response.h"
+
+namespace webserver{
+    /* 添加http头信息 */
+    void Response::add_header(int status_code,size_t content_length){
+        header<<"HTTP/1.1 "<<status_code;
+        switch(status_code){
+            case 200:header<<" OK\r\n";break;
+            case 404:header<<" NOT FOUND\r\n";break;
+            default:break;
+        }
+        header<<"Server:loin/0.1 deepin15.3\r\n";
+        header<<"Content-Length: "<<content_length<<"\r\n";
+        header<<"\r\n";
+    }
+    /* 404 not found */
+    void Response::not_found(boost::asio::ip::tcp::socket &client){
+        add_header(404,79);
+        header<<"<html><head></head><body><h1 align = 'center'>404 Not found!</h1></body></html>";
+        boost::asio::write(client,boost::asio::buffer(header.str()));       
+    }
+    /* 发送信息 */
+    void Response::send(boost::asio::ip::tcp::socket &client){
+        this->flush();
+        size_t content_length = buf.size();
+        add_header(200,content_length);
+        boost::asio::write(client,boost::asio::buffer(header.str())); 
+        boost::asio::write(client,buf); 
+    }
+}
+
+
--- response.h
+++ response.h
@@ -1,6 +1,9 @@
 /* 
  *响应类
  */
+#ifndef WEBSERVER_HEAD_RESPONSE
+#define WEBSERVER_HEAD_RESPONSE
+
 #include <iostream>
 #include <sstream>
 #include "boost/asio.hpp"
@@ -13,30 +16,11 @@ namespace webserver{
     public:
         Response():std::ostream(&buf){}
         /* 添加http头信息 */
-        void add_header(int status_code,size_t content_length){
-            header<<"HTTP/1.1 "<<status_code;
-            switch(status_code){
-                case 200:header<<" OK\r\n";break;
-                case 404:header<<" NOT FOUND\r\n";break;
-                default:break;
-            }
-            header<<"Server:loin/0.1 deepin15.3\r\n";
-            header<<"Content-Length: "<<content_length<<"\r\n";
-            header<<"\r\n";
-        }
+        void add_header(int status_code,size_t content_length);
         /* 404 not found */
-        void not_found(boost::asio::ip::tcp::socket &client){
-            add_header(404,79);
-            header<<"<html><head></head><body><h1 align = 'center'>404 Not found!</h1></body></html>";
-            boost::asio::write(client,boost::asio::buffer(header.str()));       
-        }
+        void not_found(boost::asio::ip::tcp::socket &client);
         /* 发送信息 */
-        void send(boost::asio::ip::tcp::socket &client){
-            this->flush();
-            size_t content_length = buf.size();
-            add_header(200,content_length);
-            boost::asio::write(client,boost::asio::buffer(header.str())); 
-            boost::asio::write(client,buf); 
-        }
+        void send(boost::asio::ip::tcp::socket &client);
     };
 }
+#endif
