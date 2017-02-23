e9231308be2dca86125fc008644e95c88d2b3885
liaojianqi <loinliao@163.com>
2017-02-23 16:42:58 +0800

    重构代码

--- Makefile
+++ Makefile
@@ -2,19 +2,9 @@ CC = g++
 
 default:
 	make Test
-request.o: request.h request.cpp
-	$(CC) -c request.cpp
-response.o: response.h response.cpp
-	$(CC) -c -lboost_system response.cpp
-server.o: server.h server.cpp
-	$(CC) -c -lboost_system server.cpp
-lib: request.o response.o server.o
-	make request.o
-	\make response.o
-	\make server.o
-Test: Test.cpp request.o response.o server.o
-	$(CC) -lboost_system Test.cpp request.o response.o server.o -o Test
+Test: Test.cpp request.h response.h server.h
+	$(CC) -lboost_system Test.cpp -o Test
 run: Test
 	./Test
 clean:
-	rm request.o response.o server.o Test
+	rm Test
--- response.h
+++ response.h
@@ -28,5 +28,30 @@ namespace webserver{
         /* 获取响应信息 */
         std::string get_content();
     };
+    /* 添加http头信息 */
+    void Response::add_header(std::string::size_type content_length){
+        header<<"HTTP/1.1 "<<status_code;
+        switch(status_code){
+            case 200:header<<" OK\r\n";break;
+            case 404:header<<" NOT FOUND\r\n";break;
+            default:break;
+        }
+        header<<"Server:loin/0.1 deepin15.3\r\n";
+        header<<"Content-Length: "<<content_length<<"\r\n";
+        header<<"\r\n";
+    }
+    /* 404 not found */
+    void Response::not_found(){
+        content<<"<html><head></head><body><h1 align = 'center'>"
+               <<"404 Not found!</h1></body></html>";
+        status_code = 404;
+    }
+    /* 获取响应信息 */
+    std::string Response::get_content(){
+        std::string _content = content.str();
+        add_header(_content.size());
+        header<<_content;
+        return header.str();
+    }
 }
 #endif
